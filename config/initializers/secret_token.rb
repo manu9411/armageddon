af04aa6075ae06ca5e50994a31b79a82363ec304
Manuel Molina <molin000@gmail.com>
2013-03-25 08:03:34 +0200

    1 push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Armageddon::Application.config.secret_token = '4c86f398b35e694c52b35e05510adaf6acc61e92c2de819c1fe68f9cc5669c9fd264bdd49fe17ab748f72a647666c274dfff90bf0a09fdc5398ccf6ddfd9abb1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Armageddon::Application.config.session_store :cookie_store, key: '_armageddon_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Armageddon::Application.config.session_store :active_record_store
